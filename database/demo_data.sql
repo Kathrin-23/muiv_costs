287acff9f9288ff19a99c2d4132cd25570aeca2a
Kathrin-23 <Sunkosh52@gmail.com>
2026-05-28 15:22:28 +0700

    database: add demo seed data

---
+++ database/demo_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO roles (name, title, created_at) VALUES ('admin', 'Администратор', datetime('now'));
+INSERT INTO roles (name, title, created_at) VALUES ('analyst', 'Аналитик', datetime('now'));
+INSERT INTO roles (name, title, created_at) VALUES ('user', 'Пользователь', datetime('now'));
